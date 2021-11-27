2478ec049e25f93a12e7dde9ecd4a2be720d9caf
rad2504 <94282182+rad2504@users.noreply.github.com>
2021-11-27 15:33:57 +0530

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,9 +2,11 @@
 public class FizzBuzz {
 
     public String printFizzBuzz(int value) {
+        
         if(value<1 || value>100) {
             throw new IllegalArgumentException("Input should be in range of One To Hundred");
         }
+        
         int storingCopyOfValue=value,digitIsThree=0;
         while (storingCopyOfValue>0)
         {
@@ -16,6 +18,7 @@ public class FizzBuzz {
            }
            storingCopyOfValue=storingCopyOfValue/10;
         }
+        
         int storingCopyOfValue1 =value,digitIsFive=0;
         while (storingCopyOfValue1>0)
         {
@@ -27,25 +30,26 @@ public class FizzBuzz {
             }
             storingCopyOfValue1=storingCopyOfValue1/10;
         }
+        
         if((value%3==0 &&value%5==0) || (digitIsThree==1 && digitIsFive==1))
         {
             return "FizzBuzz";
         }
+        
         else if(value%3==0 || digitIsThree==1)
         {
             return "Fizz";
         }
+        
         else if(value%5==0 || digitIsFive==1)
         {
             return "Buzz";
         }
+        
         else
         {
             return String.valueOf(value);
         }
-
-
-
 
     }
 }
