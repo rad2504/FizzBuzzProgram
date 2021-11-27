f92043f9d76d35b042ede4ea70983dfcf2502155
rad2504 <94282182+rad2504@users.noreply.github.com>
2021-11-27 15:38:21 +0530

    Update InputShouldBeLessThan100OrGreaterThan1.java

--- src/main/java/InputShouldBeLessThan100OrGreaterThan1.java
+++ src/main/java/InputShouldBeLessThan100OrGreaterThan1.java
@@ -1,7 +1,1 @@
-public class InputShouldBeLessThan100OrGreaterThan1 extends Exception {
-    public InputShouldBeLessThan100OrGreaterThan1(String exceptionToDisplay)
-    {
-        System.out.println(exceptionToDisplay);
-    }
 
-}
